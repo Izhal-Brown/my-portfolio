1cb7a594d38d0d0656c4cf92e46cf7bd80ee117c
Izhal_Brown <30791244+Izhal-Brown@users.noreply.github.com>
2023-04-19 11:59:50 +0800

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ function CTA() {
   return (
     <div className='cta'>
       {/* eslint-disable-next-line */}
-      <a href='https://drive.google.com/file/d/1m7zZs0TPfg9R_HdP3qUAILVFd9xZwv5m/view?usp=share_link' target="_blank" download className='btn'>Download CV</a>
+      <a href='https://drive.google.com/file/d/1YUT2gkvRBFJCehNkyBX4I9qJ02GvVgoN/view?usp=share_link' target="_blank" download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
